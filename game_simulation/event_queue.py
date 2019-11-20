ffa108138cd414d018bf28a5f26e1d4ce3a462e8
Alex P <alexpod1000@gmail.com>
2019-11-20 16:13:53 +0100

    Fixed event queue within same timeframe event ordering.

--- game_simulation/event_queue.py
+++ game_simulation/event_queue.py
@@ -1,8 +1,12 @@
+from itertools import count
+
 class TimeOrderedQueue:
 
     def __init__(self):
         # queue where the entries are sorted by their activation time (ascending order)
         self.queue = []
+        # event counter, used for ordering items within the same time frame
+        self.counter = count()
 
     def empty(self):
         return len(self.queue) <= 0
@@ -10,16 +14,23 @@ class TimeOrderedQueue:
     def put(self, time, item):
         n_items = len(self.queue)
         current_item = 0
+        current_id = next(self.counter)
         while current_item < n_items and time > self.queue[current_item][0]:
             current_item += 1
-        self.queue.insert(current_item, (time, item))
+        # within the same time frame, find the first position that has lower id than current id (within frame ordering)
+        while current_item < n_items and time == self.queue[current_item][0] and current_id > self.queue[current_item][1]:
+            current_item += 1
+        
+        self.queue.insert(current_item, (time, current_id, item))
 
     def peek(self):
         return self.queue[0] if not self.empty() else None
 
     def pop(self):
         # remove item from the front
-        return self.queue.pop(0)
+        item = self.queue.pop(0)
+        # return (time, event)
+        return (item[0], item[2])
 
 class EventQueue:
 
