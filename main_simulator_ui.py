9c3ab6f2e06eef1e1892e7f8b3663b0687bb31df
Alex P <alexpod1000@gmail.com>
2019-11-30 23:21:22 +0100

    Updated the UI example to include external ability loading.

--- main_simulator_ui.py
+++ main_simulator_ui.py
@@ -4,6 +4,7 @@ from PyQt5.QtCore import *
 
 import sys
 import os
+import random as random
 
 from game_model import player, ability
 from game_model.model import GameModel
@@ -11,14 +12,13 @@ import game_events.game_events as events
 from game_events.game_event_emitters import AbilityCastEventEmitter, EveryoneRestoreEventEmitter
 from simulation.simulation import DiscreteEventsSimulation
 from game_visualizers.textual_visualizer import TextualVisualizer
-import random as random
+from game_utils.utils import load_abilities
 
-flash_heal_spell = ability.HealingAbility(80, 50, cast_time=1, name="Flash Heal")
-frost_bolt_spell = ability.DamagingAbility(100, 200, cast_time=5, name="Frost Bolt")
-melee_attack_ability = ability.DamagingAbility(50, 1, cast_time=1, name="Melee Attack")
+abilities = load_abilities('game_data/abilities_db.json')
+print(abilities)
 
-player_1_abilities = [frost_bolt_spell]
-boss_abilities = [melee_attack_ability]
+player_1_abilities = [abilities[1]]
+boss_abilities = [abilities[2]]
 
 player_1 = player.Player(500, 1000, abilities=player_1_abilities, health_per_unit_time=1, mana_per_unit_time=1, name="Alex", game_class="Mage")
 boss_player = player.Player(1000, 100, abilities=boss_abilities, name="Boss", game_class="Warrior")
@@ -39,13 +39,6 @@ game_simulation = DiscreteEventsSimulation(
     ], 
     visualizers=[TextualVisualizer()]
 )
-
-# manually dispatch some events
-# dispatch initial event that starts health and resources restoration for everyone
-game_simulation.dispatch_event(events.EveryoneRestoreHealthAndResources(game_model), 0)
-game_simulation.dispatch_event(events.AbilityCastStarted(player_1, {"ability": frost_bolt_spell, "target": boss_player}), 2)
-game_simulation.dispatch_event(events.AbilityCastStarted(boss_player, {"ability": melee_attack_ability, "target": player_1}), 2)
-game_simulation.dispatch_event(events.AbilityCastStarted(player_1, {"ability": frost_bolt_spell, "target": boss_player}), 4)
 
 class SimulationWindow(QMainWindow):
     
@@ -90,10 +83,14 @@ class SimulationWindow(QMainWindow):
 
         # players heal buttons
         self.player_heal_buttons = []
+        self.player_health_bars = []
         for player in self.model.players:
             player_heal_button = QPushButton()
+            player_health_bar = QProgressBar()
             player_heal_button.pressed.connect(lambda player=player: self.send_heal(player))
+            self.player_health_bars.append(player_health_bar)
             self.player_heal_buttons.append(player_heal_button)
+            layout.addWidget(player_health_bar)
             layout.addWidget(player_heal_button)
 
         
@@ -106,16 +103,23 @@ class SimulationWindow(QMainWindow):
         self.setBaseSize(400, 200)
 
     def _update_player_health_buttons(self):
-        for player_heal_button, player in zip(
-            self.player_heal_buttons, self.model.players
+        for player_heal_button, player_health_bar, player in zip(
+            self.player_heal_buttons, self.player_health_bars, self.model.players
         ):
             if player.health > 0:
+                current_health_percentage = int((player.health / player.max_health) * 100)
+                # update bar
+                player_health_bar.setFormat("%p%")
+                player_health_bar.setValue(current_health_percentage)
+                # update button
                 player_heal_button.setText("{}\n{}/{}".format(player.name, player.health, player.max_health))
             else:
+                player_health_bar.setFormat("Dead")
+                player_health_bar.setValue(0)
                 player_heal_button.setText("{}\nDead".format(player.name))
 
     def send_heal(self, player):
-        self.simulation.dispatch_event(events.AbilityCastStarted(self.model.god, {"target": player, "ability": flash_heal_spell}))
+        self.simulation.dispatch_event(events.AbilityCastStarted(self.model.god, {"target": player, "ability": abilities[3]}))
 
     def simulator_tick(self):
         self.simulation_time_label.setText(
